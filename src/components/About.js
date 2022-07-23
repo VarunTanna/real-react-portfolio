fb50c110308eb9e87ac6184d041ee015d019ce80
Varun Tanna <varun_tanna@ymail.com>
2022-07-23 14:59:30 -0400

    app, about me and header js done

---
+++ src/components/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function AboutMe () {
+    return (
+        <div>
+            <p>Full Stack Web Developer from Charlotte, NC.</p>
+            <hr />
+            <img alt=""></img>
+            <p>Innovative and deadline-driven Web Developer with one year of experience designing and developing user-centered applications.</p>
+            <p>I have always been a critical thinker and problem solver. I recently was able to enhance these skills from working within a team based environment at the UNC Charlotte coding bootcamp. Along with critical thinking, I was able to sharpen many other skills during this time. An area of skills that stand out the most would be front end web development. After entering the bootcamp already having experience in this area, I was able to improve my existing skills including my ability to write cleaner JavaScript, and gaining a better understanding of the fundamentals of front end web development.</p>
+        </div>
+    )
+}
+
+export default AboutMe;
---
+++ src/components/Header.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import Navigation from "./Nav";
+import About from "./About";
+import Contact from "./Contact";
+import Portfolio from "./Portfolio";
+import Resume from "./Resume";
+
+function Header() {
+    const [currentPage, handlePageChange] = useState("About");
+    // render method w switch cases to render current page
+    const renderPage = () => {
+        switch (currentPage) {
+            case "About":
+                return <About />;
+            case "Portfolio":
+                return <Portfolio />;
+            case "Contact":
+                return <Contact />;
+            case "Resume": 
+                return <Resume />;
+            
+            default: 
+            return <About />;
+        }
+    };
+
+    return (
+        <div>
+            <nav className='navbar'>
+                <div>
+                <span className='bg-dark text-light'> Varun Tanna</span>
+                </div>
+            </nav>
+            <Navigation 
+            currentPage={currentPage}
+            handlePageChange={handlePageChange}
+        />
+        <main>
+            <div>{renderPage(currentPage)}</div>
+        </main>
+        </div>
+        
+
+    );
+}
+
+export default Header;
